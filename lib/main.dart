47e535701850b8015b9d98363c80cc034236a666
Sébastien bellon <sebastien.bellon@nagra.com>
2022-06-24 17:11:32 +0200

    ♻️ created arrow function

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(const XylophoneApp());
-}
+void main() => runApp(const XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({Key? key}) : super(key: key);
